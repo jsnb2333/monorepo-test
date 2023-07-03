1c0afb45aceaab36c3eb9b93eb4b84f6c364b5e0
jsnb2333 <1357930097@qq.com>
2023-07-03 23:28:21 +0800

    ✨ feat:测试组件库能否使用

---
+++ packages/components/Test/Test.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(1);
+
+function addCount() {
+  count.value++;
+}
+</script>
+
+<template>
+  <button @click="addCount">test{{ count }}</button>
+</template>
+
+<style scoped></style>
--- packages/components/index.js
+++ packages/components/index.js
@@ -1,19 +1,12 @@
-import { BsIcons, } from './BsIcons';
-import { ShImg } from './ShImg';
-import { ShIconFont, ShInfo } from './ShIcon';
-import { TableButtons } from './BsTable';
-import { Login } from './Login';
-import { BaseLayout, Title, Content, Header } from './Layout';
+// import { BsIcons, } from './BsIcons';
+// import { ShImg } from './ShImg';
+// import { ShIconFont, ShInfo } from './ShIcon';
+// import { TableButtons } from './BsTable';
+// import { Login } from './Login';
+// import { BaseLayout, Title, Content, Header } from './Layout';
+import { Test } from './Test';
 
 export {
-  BsIcons,
-  ShImg,
-  ShIconFont,
-  ShInfo,
-  TableButtons,
-  Login,
-  BaseLayout,
-  Title,
-  Content,
-  Header,
+
+  Test,
 };
